632f236cdde79e409b43301e278f11ad3f365662
Daniel Estevan Garcia Niño <104698156+EstevanGN@users.noreply.github.com>
2023-11-13 10:21:47 -0500

    Compound data readed

--- app/app.R
+++ app/app.R
@@ -396,24 +396,29 @@ ui <- navbarPage("Metabolomic Search",
         sidebarPanel(
                  
           h1("Compound Data"),
+          br(),
+          h2("General configuration "),
           p("Choose your compound data from a file with 
           CSV format (only). Press ", 
           strong("'Browse...'"), 
           " button to upload data."),
                  
-          #### Browse compound button ------------------------------------------
-          fileInput("compoundDataFile", 
-            h4("Choose CSV File:"),
-            accept = c("text/csv",
-                       "text/comma-separated-values,text/plain",
-                       ".csv"
+          #### Browse data button ----------------------------------------------
+          fileInput(
+            "compoundFile", 
+            h4("Choose CSV File"),
+            accept = c(
+              "text/csv",
+              "text/comma-separated-values,text/plain",
+              ".csv"
             )
           ),
           #### / ---------------------------------------------------------------
                  
           fluidRow(
             #### Header option -------------------------------------------------
-            column(5,
+            column(
+              5,
               h4("Header:"),
               checkboxInput(
                 "compoundHeader", 
@@ -424,24 +429,28 @@ ui <- navbarPage("Metabolomic Search",
             #### / -------------------------------------------------------------
             
             #### Decimal option ------------------------------------------------
-            column(5,
-                   radioButtons("compoundDec", 
-                                h4("Decimal style:"),
-                                c(Point=".",
-                                  Comma=","),
-                                ","
-                   ),
+            column(
+              5,
+              radioButtons(
+                "compoundDec", 
+                h4("Decimal style:"),
+                c(Point=".",
+                  Comma=","),
+                "."
+              ),
             )
             #### / -------------------------------------------------------------
           ),
                  
           fluidRow(
             #### Quote option --------------------------------------------------
-            column(5,
-              radioButtons("compoundQuote", 
+            column(
+              5,
+              radioButtons(
+                "compoundQuote", 
                 h4("Quote:"),
                 c("Double Quote"="\"",
-                  None="",
+                  "None"="",
                   "Single Quote"="'"),
                 "\""
               ),
@@ -449,16 +458,18 @@ ui <- navbarPage("Metabolomic Search",
             #### / -------------------------------------------------------------
             
             #### Separator option ----------------------------------------------
-            column(5,
-                   radioButtons("compoundSeparator", 
-                                h4("Separator character:"),
-                                c("Semicolon"=";",
-                                  "Space"=" ",
-                                  "Comma"=",",
-                                  "Tab"="\t",
-                                  "Vertical Line |"="|"),
-                                ";"
-                   )
+            column(
+              5,
+              radioButtons(
+                "compoundSep", 
+                h4("Separator character:"),
+                c("Semicolon"=";",
+                  "Space"=" ",
+                  "Comma"=",",
+                  "Tab"="\t",
+                  "Vertical Line |"="|"),
+                ";"
+              ),
             )
             #### / -------------------------------------------------------------
           ),
@@ -476,70 +487,80 @@ ui <- navbarPage("Metabolomic Search",
             )
             #### / -------------------------------------------------------------
           ),
-          
+                 
           h2("Columns configuration"),
           p("Edit the following parameters to configure the columns of 
             your data frame. For each title give a column number"),
-          
+                 
           fluidRow(
             #### Columns configuration -----------------------------------------
             tags$head(
               tags$style(
                 type="text/css", 
-                "#inline label{ display: table-cell; 
-                text-align: center; vertical-align: middle; } 
-                #inline .form-group { display: table-row;}")
+                     "#inline label{ display: table-cell; 
+                      text-align: center; vertical-align: middle; } 
+                      #inline .form-group { display: table-row;}"
+              )
             ),
-            
+                   
             tags$div(
               id = "inline", 
+              
               numericInput(
-                "compoundIDColumn", 
-                "Compound IDs:",
+                "compoundRowNamesColumn", 
+                "Row names:",
                 value = 1,
                 min = 1,
                 step = 1
               ),
+              
               numericInput(
-                "compoundMZColumn", 
+                "compoundMzValueColumn",
                 "m/z value:",
-                value = 2,
-                min = 1,
-                step = 1
-              ),
-              numericInput(
-                "compoundRTColumn", 
-                "RT value:",
                 value = 3,
                 min = 1,
                 step = 1
               ),
+              
               numericInput(
-                "compoundFragColumn", 
-                "Fragmentation pattern:",
+                "compoundRtValueColumn", 
+                "RT value:",
                 value = 4,
                 min = 1,
                 step = 1
+              ),
+              
+              numericInput(
+                "compoundFragmentsColumn", 
+                "Fragments:",
+                value = 5,
+                min = 1,
+                step = 1
               )
+              
             )
             #### / -------------------------------------------------------------
           ),
-          
+                 
           br(),
           br(),
-          
+               
           #### UNAL Logo -------------------------------------------------------
           img(src = "logo_unal.png", height="70%", width="70%")
           #### / ---------------------------------------------------------------
         ),
                
-        #### Present data uploaded --------------------------------------------- 
         mainPanel(
+          #### Present example of data format ---------------------------------- 
+          h3("Example of a data table format"),
+          img(src = "Compound data example.png", height="100%", width="100%"),
+          
+          #### Present data uploaded -------------------------------------------
           h1("Data Uploaded"),
           br(),
           br(),
           DTOutput(
-            "compoundData"
+            "compoundReaded"
           )
         )
         #### / -----------------------------------------------------------------
@@ -983,34 +1004,57 @@ server <- shinyServer(function(input, output) {
   
   
   ## Read compound data --------------------------------------------------------
-  compound_data <- reactive({
-    req(input$compoundDataFile,
+  compoundData <- reactive({
+    req(input$compoundFile,
         input$compoundHeader,
-        input$compoundSeparator,
+        input$compoundSep,
         input$compoundQuote,
         input$compoundDec,
-        input$compoundRowStart)
+        input$compoundRowStart,
+        input$compoundRowNamesColumn,
+        input$compoundMzValueColumn,
+        input$compoundRtValueColumn,
+        input$compoundFragmentsColumn)
     
-    compoundInFile <- input$compoundDataFile
+    inFile <- input$compoundFile
     
-    compoundDF <- read.table(
-      compoundInFile$datapath,
+    show_modal_spinner(
+      spin = "cube-grid",
+      color = "#4372AA",
+      text = "Reading data..."
+    )
+    df <- read.table(
+      inFile$datapath,
       header = input$compoundHeader,
-      sep = input$compoundSeparator,
+      sep = input$compoundSep,
       quote = input$compoundQuote,
       dec = input$compoundDec,
       skip = input$compoundRowStart-1
     )
+    remove_modal_spinner()
     
-    return(compoundDF)
+    return(df)
   })
   ## / -------------------------------------------------------------------------
   
   
-  ## Print compound data -------------------------------------------------------
-  output$compoundData <- renderDT(
-    compound_data()
-  )
+  ## Print compound data ---------------------------------------------------------
+  output$compoundReaded <- renderDT({
+    dt <- datatable(
+      compoundData(),
+      extensions = 'Buttons',
+      options = list(
+        dom = 'Blfrtip',
+        buttons = c('copy', 'csv', 'excel', 'pdf'),
+        columnDefs = list(
+          list(orderable=TRUE, targets=0)
+        )
+      ),
+      class = "display"
+    )
+    dt$x$data[[1]] <- as.numeric(dt$x$data[[1]]) 
+    dt
+  })
   ## / -------------------------------------------------------------------------
   
 
